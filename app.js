27535b47900202adacf0f1b717edaeb405c02bb0
stabishali8 <stabishali8@gmail.com>
2020-11-08 22:11:31 +0500

    initial comment with basic structure and dependency

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const app = express();
+
+
+const {login,refresh} = require('./authentication');
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/login',login);
+app.post('/refresh',refresh);
+
